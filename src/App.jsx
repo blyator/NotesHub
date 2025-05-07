ada1eea3ff20b4b64ae37d445d50a7a167ff14c5
Billy Yator <dmnbilly@gmail.com>
2025-05-07 11:43:34 +0300

    Code ready for Production

--- src/App.jsx
+++ src/App.jsx
@@ -49,12 +49,29 @@ export default function App() {
     })
       .then((res) => res.json())
       .then((updatedNote) => {
-        setNotes((prevNotes) =>
-          prevNotes.map((note) => (note.id === id ? updatedNote : note))
-        );
-        toast.success("Edit complete — you're on fire ⚡", {
-          className: "alert alert-success",
+        setNotes((prevNotes) => {
+          const filteredNotes = prevNotes.filter((note) => note.id !== id);
+          return [updatedNote, ...filteredNotes];
         });
+
+        toast.custom(
+          <div role="alert" className="alert alert-success">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 shrink-0 stroke-current"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>Update successful</span>
+          </div>
+        );
       })
       .catch((err) => console.error("error:", err));
   };
@@ -79,7 +96,15 @@ export default function App() {
 
   return (
     <div className="w-full mx-0 lg:max-w-none min-h-screen flex flex-col bg-base-100">
-      <Toaster position="top-right" />
+      <Toaster
+        position="top-center"
+        reverseOrder={true}
+        gutter={8}
+        toastOptions={{
+          duration: 1000,
+          removeDelay: 1000,
+        }}
+      />
       <Navbar search={search} setSearch={setSearch} />
       <main className="p-2 flex-grow">
         <Routes>
