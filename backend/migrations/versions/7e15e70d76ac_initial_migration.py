039b7ca187404a8a0763a6a43fc4dd134fe33f68
Billy Yator <dmnbilly@gmail.com>
2025-06-29 11:53:10 +0300

    fixed badge color issues

--- backend/migrations/versions/7e15e70d76ac_initial_migration.py
+++ backend/migrations/versions/7e15e70d76ac_initial_migration.py
@@ -1,8 +1,8 @@
-"""new
+"""Initial migration
 
-Revision ID: 4ad8cae14c45
+Revision ID: 7e15e70d76ac
 Revises: 
-Create Date: 2025-06-26 00:08:05.778190
+Create Date: 2025-06-27 22:53:03.408237
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '4ad8cae14c45'
+revision = '7e15e70d76ac'
 down_revision = None
 branch_labels = None
 depends_on = None
